c7ecdcca76800fa9f5185122ed99ecd9e4c3ccc9
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-03-30 17:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
-  // write your code here
+	let res = '';
+	let char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+	for(let i = 0; i < l; i++){
+		res += char_list.charAt(
+			Math.floor(
+				Math.random()*char_list.length
+			)
+		)
+	}
+	return res;
 }
 
 // Do not change the code below.
